f88e0a47e1245c18bf340924ed53ee2b1676e5df
Bhabani Shankar Jena <bhabanijena04@gmail.com>
2025-09-02 00:19:44 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors({
     origin: [
-        'http://localhost:5173', 
-        'http://localhost:5174', 
+        'http://localhost:5173',
+        'http://localhost:5174',
         'http://localhost:4000',
-        'https://forever-frontend-lovat.vercel.app',
-        'https://forever-frontend.vercel.app'
+        'https://forever-bsj04.vercel.app',
+        'https://forever-admin-bsj04.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
